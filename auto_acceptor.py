71df0ce6ca793735cebbd1d8eeb4fa81ccb67d5d
Pratik Shivhare <47967331+pratikshivhare26@users.noreply.github.com>
2019-03-17 20:04:24 +0530

    Update auto_acceptor.py

--- auto_acceptor.py
+++ auto_acceptor.py
@@ -8,6 +8,9 @@ url='https://www.linkedin.com/mynetwork/invitation-manager/?filterCriteria=ALL'
 webbrowser.open(url, new=new)
 time.sleep(3)
 i=0
+
+#keep the value of i according to the number of request we want to accept.
+
 while(i<8):
     pyautogui.click(882,416)
     time.sleep(1)
