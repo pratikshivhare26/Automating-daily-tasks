5d8eb9cb3bfd879b77f53f5da5bcb4aa99ab2828
Pratik Shivhare <47967331+pratikshivhare26@users.noreply.github.com>
2019-03-18 20:54:48 +0530

    Update auto_acceptor.py

--- auto_acceptor.py
+++ auto_acceptor.py
@@ -12,8 +12,11 @@ i=0
 #keep the value of i according to the number of request we want to accept.
 
 while(i<8):
+    #the value of pixel below is as per the position of accept button on linkedIn as per mac 13inch model.
+    #hope this wont change change or affect for any similar size model
+    #just in case it does then ping me for determ
     pyautogui.click(882,416)
-    time.sleep(1)
+    time.sleep(1.1)
     i+=1
 
 
